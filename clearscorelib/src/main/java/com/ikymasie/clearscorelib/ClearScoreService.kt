de9224ea727af82b021addeb752de4650ad4e576
ikagengm <ikymasie@gmail.com>
2022-01-08 11:39:02 +0200

    shared lib unit test

--- clearscorelib/src/main/java/com/ikymasie/clearscorelib/ClearScoreService.kt
+++ clearscorelib/src/main/java/com/ikymasie/clearscorelib/ClearScoreService.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ClearScoreService {
     // endpoint url
-    val baseUrl = "https://android-interview.s3.eu-west-2.amazonaws.com/"
+    val baseUrl = "https://android-interview.s3.eu-west-2.amazonaws.com"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
